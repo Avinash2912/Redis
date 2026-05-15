192f9196545d3c99e8de3fc8440012781f4b4646
Avinash Jha <avinashjha19@outlook.com>
2026-05-16 02:22:29 +0530

    chore: add a blank line for better code readability

--- 02_LoginOTP_TTL/src/index.js
+++ 02_LoginOTP_TTL/src/index.js
@@ -4,6 +4,7 @@ import Redis from "ioredis";
 const app = express();
 app.use(express.json());
 
+
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
 function otpKey(phoneNumber) {
